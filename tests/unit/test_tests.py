5487538d26c2e0fd254043de6eece88551bdfd71
liamhuber <liamhuber@greyhavensolutions.com>
2025-01-06 13:21:42 -0800

    Black check and ignore docs

--- tests/unit/test_tests.py
+++ tests/unit/test_tests.py
@@ -7,4 +7,4 @@ class TestVersion(unittest.TestCase):
     def test_version(self):
         version = pyiron_module_template.__version__
         print(version)
-        self.assertTrue(version.startswith('0'))
+        self.assertTrue(version.startswith("0"))
