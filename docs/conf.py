d9c791a7ba823beb8dc41dbc6cbbf2b1081118f8
Sam Dareska <37879103+samwaseda@users.noreply.github.com>
2025-05-08 20:45:09 +0200

    Make it ruff-compatible (#51)
    * remove unused sys
    
    * Change algorithm to check availability of ruff
    
    * Change action version

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
 import shutil
 import subprocess
@@ -141,11 +140,14 @@ modindex_common_prefix = ['vasp.', 'base.', 'lammps.',
 #    raise ImportError("You need to install bootstrap: pip install sphinx_bootstrap_theme")
 #    html_theme = 'default'
 
-try:
-   import sphinx_rtd_theme
-   html_theme = 'sphinx_rtd_theme'
-except ImportError:
-   html_theme = 'default'
+import importlib.util
+
+if importlib.util.find_spec("sphinx_rtd_theme") is not None:
+    # sphinx_rtd_theme is available
+    html_theme = "sphinx_rtd_theme"
+else:
+    # fallback theme
+    html_theme = "alabaster"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
