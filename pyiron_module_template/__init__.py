fbd29d0d681ff40ea70559c9e11ae00df52ec691
liamhuber <liamhuber@greyhavensolutions.com>
2025-10-02 11:25:35 -0700

    Use hatchling as build system
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_module_template/__init__.py
+++ pyiron_module_template/__init__.py
@@ -1,3 +1,8 @@
-from . import _version
+import importlib.metadata
 
-__version__ = _version.get_versions()["version"]
+try:
+    # Installed package will find its version
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    # Repository clones will register an unknown version
+    __version__ = "0.0.0+unknown"
